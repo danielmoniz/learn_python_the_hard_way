5fd9c72acc1e481bf187e40c0e80c0143ed584a3
Daniel Moniz <daniel.moniz@gmail.com>
2012-03-18 15:35:04 -0400

    Complete exercise 11 with a few mistakes.

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're {!r} old, {!r} tall and {!r} heavy.".format(age, height, weight)
+
+print "Who is your daddy, and what does he do?",
+daddy = raw_input()
+
+print "So your daddy is {!r}".format(daddy)
