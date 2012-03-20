398dc6913d412774e85aecf8541b71592347719a
Daniel Moniz <daniel.moniz@gmail.com>
2012-03-19 22:55:38 -0400

    Complete exercise 32

---
+++ ex32.py
@@ -0,0 +1,32 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print "This is count {}".format(number)
+
+# same as above
+for fruit in fruits:
+    print "A fruit of type: {}".format(fruit)
+
+# also we can go through mixed lists too
+# notice we have to use !r since we don't know what's in it
+for i in change:
+    print "I got {!r}".format(i)
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print "Adding {} to the list.".format(i)
+# append is a function that lists understand
+    elements.append(i)
+
+# OR, easier:
+elements = range(0, 6)
+
+# now we can print them out too
+for i in elements:
+    print "Element was: {}".format(i)
